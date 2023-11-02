2a86a8423d4f9ccdc5ddfacd43f1fe2e2919c943
Muhammad Fahrezi Syahputra <muhammadfahrezisyahputra@gmail.com>
2023-11-02 14:34:02 +0700

    data validation implemented

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,5 +1,8 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-underscore-dangle */
 const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
   constructor() {
@@ -25,7 +28,7 @@ class NotesService {
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
     if (!isSuccess) {
-      throw new Error('Catatan kayanya masih belom bisa ditambahin dah');
+      throw new InvariantError('Catatan gagal ditambahkan');
     }
 
     return id;
@@ -38,9 +41,8 @@ class NotesService {
   getNoteById(id) {
     const note = this._notes.filter((n) => n.id === id)[0];
     if (!note) {
-      throw new Error('Catatan tidak ditemukan');
+      throw new NotFoundError('Catatan tidak ditemukan');
     }
-
     return note;
   }
 
@@ -48,7 +50,7 @@ class NotesService {
     const index = this._notes.findIndex((note) => note.id === id);
 
     if (index === -1) {
-      throw new Error('Ini gagal nih memperbarui catatan');
+      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
     }
 
     const updatedAt = new Date().toISOString();
@@ -64,11 +66,9 @@ class NotesService {
 
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
-
     if (index === -1) {
-      throw new Error('Catatan gagal dihapus nih. Idnya gaada kali ini');
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
     }
-
     this._notes.splice(index, 1);
   }
 }
---
+++ src/validator/notes/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const InvariantError = require('../../exceptions/InvariantError');
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
