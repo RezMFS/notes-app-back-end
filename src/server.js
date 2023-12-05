d188e77d1d74895779a5950dec92d29c87357db0
Muhammad Fahrezi Syahputra <muhammadfahrezisyahputra@gmail.com>
2023-12-05 11:35:09 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
+// notes
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesServices');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
+  const usersService = new UsersService();
   const notesService = new NotesService();
   const server = Hapi.server({
     port: process.env.PORT,
@@ -17,13 +24,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
